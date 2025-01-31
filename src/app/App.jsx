d2af57424991082912f5242ece9485b00d2dfdf7
MishraJuhi9125 <juhimishra9125@gmail.com>
2025-01-31 19:50:30 +0530

    update

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,27 +1,4 @@
-// import { useState } from 'react'
-// import Login from '../pages/Login'
-// import Dashboard from '../pages/Dashboard'
-// import Sidebar from '../components/Sidebar'
-// import Students from '../pages/Students'
-// import Home from '../pages/home'
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//      <div>
-//      {/* <Login/> */}
-//      <Dashboard/>
-//      {/* <Sidebar/> */}
-//      {/* <Students/> */}
-//      {/* <Home/> */}
-//      </div>
-//     </>
-//   )
-// }
-
-// export default App
 import { Routes, Route } from 'react-router-dom'; 
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
